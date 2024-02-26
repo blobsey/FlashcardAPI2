b3dbcfb47bd1899d3a0860c4e58bd5705af6ea84
blobsey <blobs03@gmail.com>
2024-02-26 19:32:16 +0000

    update deploy.sh to use a new venv every time

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-# Define variables
-FUNCTION_NAME="FlashcardAPIv2"
-VENV_DIR="venv"
-PACKAGE_FILE="package.zip"
-DEPLOYMENT_DIR="deployment"
+{
+    # Define variables
+    FUNCTION_NAME="FlashcardAPIv2"
+    VENV_DIR="venv"
+    PACKAGE_FILE="package.zip"
+    DEPLOYMENT_DIR="deployment"
+    PYTHON_VERSION="python3.9" # Adjust as needed
 
-# Activate the virtual environment
-source $VENV_DIR/bin/activate
+    # Install dependencies from requirement.txt into new venv
+    rm -rf $VENV_DIR
+    $PYTHON_VERSION -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
+    pip install -r requirements.txt
 
-# Install dependencies
-pip install -r requirements.txt
+    # Create a deployment directory
+    mkdir -p $DEPLOYMENT_DIR
+    cd $DEPLOYMENT_DIR
+    rm -f $PACKAGE_FILE # Remove any existing package file
 
-# Create a deployment directory
-mkdir -p $DEPLOYMENT_DIR
-cd $DEPLOYMENT_DIR
+    # Add dependencies to the package
+    cd ../$VENV_DIR/lib/python3.*/site-packages/
+    zip -r9 ../../../../$DEPLOYMENT_DIR/$PACKAGE_FILE .
 
-# Remove any existing package file
-rm -f $PACKAGE_FILE
+    # Add your application code to the package
+    cd ../../../../
+    zip -g $DEPLOYMENT_DIR/$PACKAGE_FILE main.py
 
-# Add dependencies to the package
-cd ../$VENV_DIR/lib/python3.*/site-packages/
-zip -r9 ../../../../$DEPLOYMENT_DIR/$PACKAGE_FILE .
+    # Deactivate the virtual environment
+    deactivate
 
-# Add your application code to the package
-cd ../../../../
-zip -g $DEPLOYMENT_DIR/$PACKAGE_FILE main.py
+    # Update the Lambda function
+    aws lambda update-function-code \
+        --function-name $FUNCTION_NAME \
+        --zip-file fileb://$DEPLOYMENT_DIR/$PACKAGE_FILE
 
-# Deactivate the virtual environment
-deactivate
-
-# Update the Lambda function
-aws lambda update-function-code \
-    --function-name $FUNCTION_NAME \
-    --zip-file fileb://$DEPLOYMENT_DIR/$PACKAGE_FILE
-
-# Clean up the deployment directory if you don't need it anymore
-rm -rf $DEPLOYMENT_DIR
+    # Clean up the deployment directory if you don't need it anymore
+    rm -rf $DEPLOYMENT_DIR
+} >> "deploy.log" 2>&1
 
 echo "Deployment complete."
-
